2a6aa6b3eb951d6032b7e033bd051bc728d64dc5
Neil <neil@takaworx.com>
2019-01-31 09:56:48 +0800

    added spinner to form

--- src/js/components/form.vue
+++ src/js/components/form.vue
@@ -1,17 +1,37 @@
 <template>
-    <form @submit.prevent="submit" :autocomplete="autocomplete">
+    <form @submit.prevent="submit" :autocomplete="autocomplete" class="vme-form">
         <fieldset :disabled="onTransit">
             <slot></slot>
         </fieldset>
+        <div v-if="showSpinner" class="spinner-backdrop">
+            <div class="preloader-wrapper active">
+                <div class="spinner-layer spinner-red-only">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </form>
 </template>
 
 <script>
 export default {
-    props: [
-        'autocomplete',
-        'params',
-    ],
+    data() {
+        return {
+            data: null,
+            onTransit: false,
+        }
+    },
+    created() {
+        this.data = this.setData(this.params);
+    },
     methods: {
         setData(data) {
             this.data = data;
@@ -27,14 +47,19 @@ export default {
             this.$emit('submit', this.setTransitFalse, this.data);
         }
     },
-    data() {
-        return {
-            data: null,
-            onTransit: false,
+    computed: {
+        showSpinner() {
+            if (this.spinner == null || ! this.spinner) {
+                return false
+            }
+
+            return this.onTransit
         }
     },
-    created() {
-        this.data = this.setData(this.params);
-    }
+    props: [
+        'autocomplete',
+        'spinner',
+        'params',
+    ],
 }
 </script>
